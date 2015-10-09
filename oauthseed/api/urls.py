1704a66cd0bf4aac8d69430b07162b9039303c6b
Tuss4 <tuss4dbfn@gmail.com>
2015-10-08 23:25:37 -0500

    setup twitter callback url

--- oauthseed/api/urls.py
+++ oauthseed/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     '',
     url(r'^login/?$', user_api.LoginView.as_view(), name='login'),
     url(r'^fbcallback/?$', user_api.FacebookCallBackView.as_view(), name='fbcallback'),
+    url(r'^twcallback/?$', user_api.TwitterCallBackView.as_view(), name='twcallback'),
 )
